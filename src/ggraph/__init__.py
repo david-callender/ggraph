a767e15286949e9783e4ee98e72fdffd2afb1bc8
Wade7wastaken <86390605+Wade7wastaken@users.noreply.github.com>
2026-09-21 19:33:59 -0500

    initial default window

--- src/ggraph/__init__.py
+++ src/ggraph/__init__.py
@@ -1,2 +1,36 @@
+import random
+import sys
+
+from PySide6 import QtCore, QtWidgets
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel(
+            "Hello World", alignment=QtCore.Qt.AlignmentFlag.AlignCenter
+        )
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.text)
+        layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
 def main() -> None:
-    print("Hello from ggraph!")
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
